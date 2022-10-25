68832acf55f3e65f2f777aa54cd50583239d590f
fe-ktch <sangarefantha@gmail.com>
2022-10-25 17:58:56 +0200

    fejlécek hozzáadva

--- namifesz/src/App.java
+++ namifesz/src/App.java
@@ -1,3 +1,13 @@
+/* 
+File: App.java
+Author: Sangare F. Felisha
+Copyright: 2022, Sangare F. Felisha
+Group: SZOFT II N
+Date: 2022-10-25
+Github: https://github.com/fe-ktch/J-namifesz
+Licenc: GNU GPL
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
--- namifesz/src/Nyilvantarto.java
+++ namifesz/src/Nyilvantarto.java
@@ -1,3 +1,13 @@
+/* 
+File: Nyilvantarto.java
+Author: Sangare F. Felisha
+Copyright: 2022, Sangare F. Felisha
+Group: SZOFT II N
+Date: 2022-10-25
+Github: https://github.com/fe-ktch/J-namifesz
+Licenc: GNU GPL
+*/
+
 public class Nyilvantarto {
     
 }
