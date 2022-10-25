7f0ae4c28df11b2060e006c1514aa7127c5807a1
fe-ktch <sangarefantha@gmail.com>
2022-10-25 18:07:24 +0200

    feladat kész

--- namifesz/src/App.java
+++ namifesz/src/App.java
@@ -8,8 +8,15 @@ Github: https://github.com/fe-ktch/J-namifesz
 Licenc: GNU GPL
 */
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Sangare F. Felisha, SZOFT II N, 2022-10-25");
+        Nyilvantarto nyilvantarto = new Nyilvantarto();
+        ArrayList<String> Names = nyilvantarto.getNames();
+        for(String name : Names) {
+            System.out.println(name);
+        }
     }
 }
